ed2dfb0da7f74482c614bf3087e2b0cd5f60ae54
oohbayann <akbayannabieva@gmail.com>
2026-07-03 16:46:08 +0500

    Refactor car movement logic and remove unused code

--- main.js
+++ main.js
@@ -1,32 +1,13 @@
 let p1Pos = 0;
-let targetP1 = 0;
+let p2Pos = 0;
 
-const startBtn = document.getElementById('start-game');
-startBtn.addEventListener('click', async () => {
-    document.getElementById('lobby').style.display = 'none';
-    document.getElementById('game-arena').style.display = 'block';
-    
-    // Микрофонды қосу
-    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-    const audioContext = new AudioContext();
-    const source = audioContext.createMediaStreamSource(stream);
-    const analyser = audioContext.createAnalyser();
-    source.connect(analyser);
-    analyser.fftSize = 256;
-    const bufferLength = analyser.frequencyBinCount;
-    const dataArray = new Uint8Array(bufferLength);
-
-    function animate() {
-        analyser.getByteFrequencyData(dataArray);
-        let sum = dataArray.reduce((a, b) => a + b);
-        let avg = sum / bufferLength;
-
-        // Машинаны жылжыту (Lerp)
-        targetP1 = avg * 3;
-        p1Pos += (targetP1 - p1Pos) * 0.05;
+// Бұл функцияны WebSocket-тен келген деректер арқылы шақырасыз
+function moveCar(playerId, volume) {
+    if (playerId === 'p1') {
+        p1Pos += volume * 2;
         document.getElementById('p1').style.left = p1Pos + "px";
-
-        requestAnimationFrame(animate);
+    } else {
+        p2Pos += volume * 2;
+        document.getElementById('p2').style.left = p2Pos + "px";
     }
-    animate();
-});
+}
